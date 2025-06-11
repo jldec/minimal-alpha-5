f4b8957929f91c37f04d958501cec3ea2ffc2d19
jldec <jurgen@jldec.me>
2025-06-11 22:29:20 +0100

    useAgent param and path must match binding name

--- src/app/pages/Agent.tsx
+++ src/app/pages/Agent.tsx
@@ -4,26 +4,23 @@ import { MessageInput } from '../shared/MessageInput'
 import { ChatLayout } from '../shared/ChatLayout'
 import { useAgent } from 'agents/react'
 import { useAgentChat } from 'agents/ai-react'
-import { Message } from '@/app/shared/askAI'
 
 export function Agent() {
   const agent = useAgent({
-    agent: 'chat',
+    agent: 'chat-agent',
     name: 'agent'
   })
 
   const { messages, handleSubmit, clearHistory, setInput } = useAgentChat({ agent })
 
   const onSubmit = async (prompt: string) => {
-    // setInput(prompt)
-    // handleSubmit()
+    setInput(prompt)
+    handleSubmit()
   }
 
   const onClear = async () => {
-    // clearHistory()
+    clearHistory()
   }
-
-  // const messages: Message[] = []
 
   return (
     <ChatLayout title="RedwoodSDK Agent Chat">
