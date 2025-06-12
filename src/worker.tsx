1e763d04f6e7fcc3c6793d3a1f7be2599683c393
justinvdm <justinvderm@gmail.com>
2025-06-12 03:56:11 +0200

    Git agents working with rwsdk

--- src/worker.tsx
+++ src/worker.tsx
@@ -11,9 +11,11 @@ export { ChatAgentDO } from '@/app/ChatAgentDO'
 export type AppContext = {}
 
 export default defineApp([
-  render(Document, [index([App])]),
-
-  route(`/agents/${env.CHAT_AGENT_NAMESPACE_KEBABCASE}/${env.CHAT_AGENT_ID}`, async ({ request }) => {
-    return (await routeAgentRequest(request, env)) || Response.json({ msg: 'no agent here' }, { status: 404 })
-  })
+  async ({ request }) => {
+    const response = await routeAgentRequest(request, env)
+    if (response) {
+      return response
+    }
+  },
+  render(Document, [index([App])])
 ])
